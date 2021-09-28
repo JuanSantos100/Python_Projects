9ba323d0dbef872a00f0a7527af977d8a5d64947
Juan Victor Ventura Santos <juanvictortimaster@gmail.com>
2021-09-28 20:35:30 -0300

    Configurando comandos básicos. Estruturando o projeto

---
+++ basicos/comandos_basicos.py
@@ -0,0 +1,26 @@
+"""
+Comentários para maior quantidade de linhas
+"""
+
+"""
+Comandos prints
+"""
+print('Hello World')
+
+
+#sep='' irá separar os itens do print de acordo com o que for inserido
+#end='' acrescenta o caractere ao final do meu conteúdo do print()
+print('824', '176', '070', sep='.', end='-') 
+print('Esse é o meu \'texto\' (str)')
+print(r'this is my \n text')  #r permite que nenhum código seja executado entre ''
+
+
+
+"""
+Tipos primitivos
+"""
+
+print('Luiz', type('Luiz'))
+print(10, type(10))
+print(25.23, type(25.23))
+print('l' == 'L', type('l' == 'L')) 
