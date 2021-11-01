650acbae617a769793382672247cff750fb82a76
Juan Victor Ventura Santos <juanvictortimaster@gmail.com>
2021-11-01 11:14:22 -0300

    Crie uma função que exibe uma saudação com os parâmetros saudação e nome

---
+++ exercicios/funcoes/exercicio1.py
@@ -0,0 +1,13 @@
+"""
+Crie uma função que exibe uma saudação com os parâmetros 
+saudação e nome
+"""
+
+
+def saudacao(saudacao, nome):
+    print(saudacao, nome)
+
+saudacao_usuario = 'Olá, tudo bem ? '
+nome = 'Maurício'
+
+saudacao(saudacao_usuario, nome)
