0f20a56e4d48a04617415b3307e979bc35d7a79b
HASP - Juan Victor Ventura Santos <juan.santos@ucb.org.br>
2021-11-26 14:03:09 -0300

    Estudando módulos em Python | Criando meus próprios módulos

--- basicos2/meus_modulos.py
+++ basicos2/meus_modulos.py
@@ -1,6 +1,21 @@
+import math
+
+PI = math.pi
+
 def dobra_lista(lista):
     return [x * 2 for x in lista]
 
-lista = [2,4,6,8,10]
 
-print(dobra_lista(lista))
+
+def multiplica(lista):
+    r = 1
+    for i in lista:
+        r *= i
+    return r
+
+#Todo módulo por padrão executa main
+#Condicional para impressão de testes
+if __name__ == '__main__':
+    lista = [2,4,6,8,10]
+    print(multiplica(lista))
+    print(PI)
---
+++ basicos2/usando_meus_modulos.py
@@ -0,0 +1,11 @@
+"""
+Usando o arquivo meus_modulos.py como meu próprio módulo
+"""
+
+import meus_modulos as mod
+
+print(mod.PI)
+
+lista = [2 , 4, 6, 8]
+print(mod.dobra_lista(lista))
+
